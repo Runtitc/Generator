5a5ccae6aedb62b61f45bc4b034d5c308373579f
Przemo <goblinek297@gmail.com>
2016-10-06 09:57:53 -0700

    printing directories

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Welcome");
 	    String program = "ls";
         ArrayList options = new ArrayList();
 
